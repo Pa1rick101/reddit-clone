08d1390fb37f31da968fd00338ee77bdb4e80dbd
Patrick <patrick.suehrer@thedac.info>
2022-07-02 12:35:58 +0200

    added reddit login/logout

--- Components/Header.tsx
+++ Components/Header.tsx
@@ -2,8 +2,10 @@ import Image from 'next/image'
 import React from 'react'
 import { ChevronDownIcon,  HomeIcon, MenuIcon, SearchIcon } from '@heroicons/react/solid'
 import { BellIcon, ChatIcon,PlusIcon, SpeakerphoneIcon,SparklesIcon,GlobeIcon,VideoCameraIcon, StarIcon } from '@heroicons/react/outline'
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 function Header() {
+  const { data: session} = useSession();
   return ( 
   <div className='sticky top-0 z-50 flex bg-white px-4 py-2 shadow-sm'>
     <div className='relative h-10 w-20 flex-shrink-0 cursor-pointer'>
@@ -42,7 +44,44 @@ function Header() {
     <div className='ml-5 flex items-center lg:hidden'>
       <MenuIcon className='icon' />
     </div>
-  </div>
+
+    {/* Sign in button */}
+    {session ? (
+      <div 
+        onClick={() => signOut()}
+        className='hidden items-center lg:flex space-x-2 border border-gray-100 cursor-pointer'>
+          <div className='relative h-5 w-5' flex-shrink-0>
+            <Image 
+            objectFit='contain'
+            src="https://links.papareact.com/23l" 
+            layout='fill'
+            alt=""
+            />
+        </div>
+
+        <div className='flex-1 text-xs'>
+            <p className='truncate'>{session?.user?.name}</p>
+          <p className='text-gray-400'>1 Karma</p>
+        </div>
+
+        <ChevronDownIcon className='h-5 flex-shrink-0 text-gray-400'/>
+      </div>
+    ): (
+      <div 
+        onClick={() => signIn()}
+        className='hidden items-center lg:flex space-x-2 border border-gray-100 cursor-pointer'>
+          <div className='relative h-5 w-5' flex-shrink-0>
+            <Image 
+            objectFit='contain'
+            src="https://links.papareact.com/23l" 
+            layout='fill'
+            alt=""
+            />
+          </div>
+        <p className='text-gray-400'>Sign In</p>
+      </div>
+      )}
+    </div>
   )
 }
 
