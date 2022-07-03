1c87c36984c8fbf60546f88ce91f680bc01be7e9
Patrick <patrick.suehrer@thedac.info>
2022-07-03 10:40:23 +0200

    added PostBox

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,24 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../Components/Header'
+import PostBox from '../Components/PostBox'
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <div className="my-7 mx-auto max-w-5xl">
       <Head>
         <title>Reddit 2.0</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      {/* PostBost*/}
+      <PostBox/>
+
+
+      <div className="flex">
+        {/* Feed*/}
+      </div>
+
 
     </div>
   )
